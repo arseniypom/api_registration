ca7324f86161db96c469b155e23fed27dcbc252e
arseniy_pomazkov <arpomazkov@bk.ru>
2021-01-22 10:56:32 +0300

    Added input handler to login component, changed the eye image for better quality and adjusted it styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,25 +4,66 @@ import Image from "./Image";
 
 
 function Login() {
-    const eye = <img src="images/closed_eye.png" alt=""></img>;
+    // Объект с пользовательскими данными
+    const [inputInfo, setInputInfo] = useState({
+        username: "",
+        password: ""
+    });
+    const eye = <img className="input-icon-eye_img" src="images/closed_eye.png" alt=""></img>;
+
+    const [inputStyle, setInputStyle] = useState("page-content-input");
+    function emailValidation(emailAddress) {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (re.test(emailAddress)) {
+            setInputStyle("page-content-input page-content-input-success");
+        } else {
+            setInputStyle("page-content-input page-content-input-error");
+        }
+    }
+
+
     const [passwordShown, setPasswordShown] = useState(false);
-    const togglePasswordVisiblity = () => {
+    function togglePasswordVisiblity() {
       setPasswordShown(passwordShown ? false : true);
     };
+    
+    function handleInput(event) {
+        const {name, value} = event.target;
+        if (name === "username") {
+            emailValidation(value);
+        }
+        setInputInfo((prevValue) => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        })
+    }
+
     return <div className="body">
         <Image />
         <div className="page-content">
             <p className="page-content-p">API Developer Portal</p>
             <h1 className="page-content-h1">Войти в аккаунт</h1>
-            <input type="email" name="username" className="page-content-input" placeholder="Введите логин" autoFocus></input>
+            <input
+                type="email"
+                name="username"
+                className={inputStyle}
+                placeholder="Введите логин"
+                autoFocus
+                value={inputInfo.username}
+                onChange={handleInput}
+            />
             <div className="login-password-wrapper">
                 <input
                     name="password"
                     className="page-content-input"
                     placeholder="Введите пароль"
                     type={passwordShown ? "text" : "password"}
+                    value={inputInfo.password}
+                    onChange={handleInput}
                 />
-                <i onClick={togglePasswordVisiblity}>{eye}</i>
+                <i className="input-icon" onClick={togglePasswordVisiblity}>{eye}</i>
             </div>
             <div className="page-content-actions">
                 <button className="page-content-button page-content-button-blue login-button-blue" type="submit">Войти</button>
